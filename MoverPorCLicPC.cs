5632d7841e21da580205d7a579f0b0f7e8bbfce6
F1rstree <pepe.smx@gmail.com>
2021-11-26 13:21:19 +0100

    Create MoverPorCLicPC.cs

---
+++ MoverPorCLicPC.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoverPorCLicPC : MonoBehaviour {
+
+    public GameObject objetivoParticulas;
+    public GameObject auxObjetivo;
+    public GameObject interfaz;
+    private bool animando = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !animando)
+        {
+
+            Ray rayModelo = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.tag.Equals("Terreno"))
+                {
+
+                    GetComponent<NavMeshAgent>().SetDestination(hit.point);
+
+                    transform.Find("Bruja").GetComponent<eventoOntrigger>().dragon.transform.LookAt(hit.point);
+                    transform.Find("Bruja").GetComponent<eventoOntrigger>().dragon.GetComponent<NavMeshAgent>().SetDestination(hit.point);
+
+                    if (auxObjetivo)
+                        Destroy(auxObjetivo);
+                    auxObjetivo = Instantiate(objetivoParticulas, hit.point,Quaternion.Euler(90,0,0));
+
+
+                }
+            }
+
+        }
+
+        if (auxObjetivo)
+        {
+            transform.Find("Bruja").LookAt(auxObjetivo.transform.position);
+        }
+            
+    }
+
+   
+
+
+    public void destruirParticulas()
+    {
+        Destroy(auxObjetivo);
+
+    }
+
+}
+
